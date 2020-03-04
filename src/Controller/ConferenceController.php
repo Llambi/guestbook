5e9d15251c6cf8a654d117e9d22df1a3d8b390c5
Llambi <hugopf.mail@gmail.com>
2020-03-04 14:48:16 +0100

    2 Http request better than one

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -36,11 +36,20 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $conferenceRepository)
     {
-        $response = new Response($this->twig - $this->render('conference/index.html.twig', ["conferences" => $conferenceRepository->findAll()]));
+        $response = new Response($this->twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll(),]));
         $response->setSharedMaxAge(3600);
         return $response;
     }
 
+    /**
+     * @Route("/conference_header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    {
+        return new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]));
+    }
 
     /**
      * @Route("/conference/{slug}", name="conference")
